b2dd67442fd117c5c8891d662967d96a0d52da0e
Ankit Sharma <66104733+ankitrajsh@users.noreply.github.com>
2025-01-17 13:35:27 +0530

    train

--- tiny_vit.py
+++ tiny_vit.py
@@ -14,6 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 import timm
+
 from timm.models.layers import DropPath as TimmDropPath,\
     to_2tuple, trunc_normal_
 from timm.models.registry import register_model
